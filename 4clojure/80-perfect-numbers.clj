b5a28d13393bdd9c01109f81b2e938b1bddc102c
Velko Nikolov <velko.nikolov@gmail.com>
2016-08-06 17:31:00 +0300

    4clojure 93

--- 4clojure/80-perfect-numbers.clj
+++ 4clojure/80-perfect-numbers.clj
@@ -3,7 +3,7 @@
 (letfn [(divisors [x] (filter #(= (mod x %) 0) (range 1 x)))
         (perfect? [x] (= (reduce + (divisors x)) x))]
   perfect?)
-  
+
 #(->>
   (range 1 %)
   (filter (comp zero? (partial mod %)))
--- 4clojure/82-word-chains.clj
+++ 4clojure/82-word-chains.clj
@@ -1,10 +1,51 @@
-(defn deletions [word]
+; Find out if a set of words can form a word chain
+
+(letfn
+
+[
+( deletions [word]
     (set (into [(subs word 0 (dec (count word)))]
         (map
             #(str (subs word 0 (- % 1)) (subs word %))
             (range 1 (count word))))))
 
-(defn adjacent [w1 w2]
+(adjacent [w1 w2]
     (let [d1 (deletions w1)
           d2 (deletions w2)]
-      (some identity [(d1 w2) (d2 w1) (some identity (clojure.set/intersection d1 d2))])))
+      (some boolean [(d1 w2) (d2 w1)
+        (and
+            (= (count w1) (count w2))
+            (= 1 (count (filter (partial apply #(not= %1 %2)) (map vector w1 w2)))))])))
+
+( permutations [xs]
+    (if (empty? xs)
+        []
+    (if (empty? (rest xs))
+        [[(first xs)]]
+    (loop [back (first xs)
+           left []
+           right (vec (rest xs))
+           so-far []]
+        (if (empty? right)
+            (into so-far (map #(conj % back) (permutations left)))
+            (recur
+                (first right)
+                (into left [back])
+                (rest right)
+                (into so-far (vec (map #(conj % back) (permutations (into left right)))))))))))
+
+( is-chain [words]
+    (first
+        (reduce
+            (fn [[so-far last] now]
+                (if-not so-far
+                    [so-far now]
+                    [(adjacent last now) now]))
+            [true (first words)]
+            (rest words))))
+
+( word-chain [words]
+    (boolean (some
+        is-chain
+        (permutations words))))]
+    word-chain)
